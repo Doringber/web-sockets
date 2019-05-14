3f8f7943dab6d4f81486d1485cf9382589d2cea4
shai <sigalos103@gmail.com>
2019-05-14 12:15:52 +0300

    general fixes

--- src/main/java/com/elector/WebSocketConfig.java
+++ src/main/java/com/elector/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package com.elector;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -13,14 +16,23 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import static com.elector.Utils.Definitions.KEEP_ALIVE_MESSAGE;
+import static com.elector.Utils.Definitions.MINUTE;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketConfigurer.class);
+    private static int totalSessions;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         webSocketHandlerRegistry.addHandler(new MessagesHandler(), "/stream");
+    }
+
+    @Scheduled (fixedRate = 5 * MINUTE)
+    public void logActiveSessions () {
+        LOGGER.info("current total active sessions: {}", totalSessions);
     }
 
 
@@ -28,10 +40,13 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
         private List<WebSocketSession> sessionList = new CopyOnWriteArrayList<>();
 
+
         @Override
         public void afterConnectionEstablished(WebSocketSession session) throws Exception {
             super.afterConnectionEstablished(session);
             sessionList.add(session);
+            totalSessions = sessionList.size();
+            LOGGER.info("session opened");
         }
 
         @Override
@@ -48,6 +63,9 @@ public class WebSocketConfig implements WebSocketConfigurer {
         public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
             super.afterConnectionClosed(session, status);
             sessionList.remove(session);
+            totalSessions = sessionList.size();
+            LOGGER.info("session closed");
+
         }
     }
 
